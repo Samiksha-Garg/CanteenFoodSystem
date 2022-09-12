e4d57838eb0b945ec81011bcf66472a5662ee00b
Aastha Pinhatiya <aasthapinhatia@gmail.com>
2022-09-12 21:07:08 +0530

    order summary

---
+++ lib/screens/cartScreen/tempCodeRunnerFile.dart
@@ -0,0 +1,8 @@
+import 'package:canteen_system/components/custom_app_bar.dart';
+import 'package:canteen_system/components/custom_navigation_bar.dart';
+import 'package:canteen_system/helper/constants.dart';
+import 'package:canteen_system/helper/size_config.dart';
+import 'package:canteen_system/models/Cart.dart';
+import 'package:canteen_system/screens/cartScreen/components/cart_tile.dart';
+import 'package:canteen_system/screens/cartScreen/components/place_order_card.dart';
+import 'package:flutter/material.dart';
