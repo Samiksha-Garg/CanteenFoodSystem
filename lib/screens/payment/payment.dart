e14d088b12be8c31138c802406ec05601a6acecc
Samiksha Garg <gargsamiksha0306@gmail.com>
2022-09-15 21:55:45 +0530

    minor change

--- lib/screens/payment/payment.dart
+++ lib/screens/payment/payment.dart
@@ -20,95 +20,105 @@ class Payment extends StatelessWidget {
       backgroundColor: Color.fromRGBO(48, 46, 59, 1),
       appBar: customAppBar("Payment"),
       bottomNavigationBar: const CustomBottomNavBar(),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Container(
-              padding: EdgeInsets.all(25),
-              margin: EdgeInsets.only(bottom: 10),
-              width: getProportionateScreenWidth(300),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Text(
-                'Debit/Credit Card',
-                textAlign: TextAlign.start,
-                style: kHeadingTextStyle.copyWith(color: Colors.black),
-              ),
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                SizedBox(
+                  height: getProportionateScreenHeight(10),
+                ),
+                Container(
+                  padding: EdgeInsets.all(25),
+                  margin: EdgeInsets.only(bottom: 10),
+                  width: getProportionateScreenWidth(300),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Text(
+                    'Debit/Credit Card',
+                    textAlign: TextAlign.start,
+                    style: kHeadingTextStyle.copyWith(color: Colors.black),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(25),
+                  margin: EdgeInsets.only(bottom: 10),
+                  width: getProportionateScreenWidth(300),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Text(
+                    'GPay',
+                    textAlign: TextAlign.start,
+                    style: kHeadingTextStyle.copyWith(color: Colors.black),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(25),
+                  margin: EdgeInsets.only(bottom: 10),
+                  width: getProportionateScreenWidth(300),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Text(
+                    'Paytm',
+                    textAlign: TextAlign.start,
+                    style: kHeadingTextStyle.copyWith(color: Colors.black),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(25),
+                  margin: EdgeInsets.only(bottom: 10),
+                  width: getProportionateScreenWidth(300),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Text(
+                    'PhonePay',
+                    textAlign: TextAlign.start,
+                    style: kHeadingTextStyle.copyWith(color: Colors.black),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(25),
+                  margin: EdgeInsets.only(bottom: 10),
+                  width: getProportionateScreenWidth(300),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Text(
+                    'Cash',
+                    textAlign: TextAlign.start,
+                    style: kHeadingTextStyle.copyWith(color: Colors.black),
+                  ),
+                ),
+                Container(
+                  padding: EdgeInsets.all(25),
+                  margin: EdgeInsets.only(bottom: 10),
+                  width: getProportionateScreenWidth(300),
+                  decoration: BoxDecoration(
+                    color: Color.fromRGBO(2, 51, 112, 1),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Text(
+                    '+ Add Payment Option',
+                    textAlign: TextAlign.center,
+                    style: kHeadingTextStyle,
+                  ),
+                ),
+                SizedBox(
+                  height: getProportionateScreenHeight(10),
+                ),
+              ],
             ),
-            Container(
-              padding: EdgeInsets.all(25),
-              margin: EdgeInsets.only(bottom: 10),
-              width: getProportionateScreenWidth(300),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Text(
-                'GPay',
-                textAlign: TextAlign.start,
-                style: kHeadingTextStyle.copyWith(color: Colors.black),
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.all(25),
-              margin: EdgeInsets.only(bottom: 10),
-              width: getProportionateScreenWidth(300),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Text(
-                'Paytm',
-                textAlign: TextAlign.start,
-                style: kHeadingTextStyle.copyWith(color: Colors.black),
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.all(25),
-              margin: EdgeInsets.only(bottom: 10),
-              width: getProportionateScreenWidth(300),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Text(
-                'PhonePay',
-                textAlign: TextAlign.start,
-                style: kHeadingTextStyle.copyWith(color: Colors.black),
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.all(25),
-              margin: EdgeInsets.only(bottom: 10),
-              width: getProportionateScreenWidth(300),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Text(
-                'Cash',
-                textAlign: TextAlign.start,
-                style: kHeadingTextStyle.copyWith(color: Colors.black),
-              ),
-            ),
-            Container(
-              padding: EdgeInsets.all(25),
-              margin: EdgeInsets.only(bottom: 10),
-              width: getProportionateScreenWidth(300),
-              decoration: BoxDecoration(
-                color: Color.fromRGBO(2, 51, 112, 1),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Text(
-                '+ Add Payment Option',
-                textAlign: TextAlign.center,
-                style: kHeadingTextStyle,
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );
