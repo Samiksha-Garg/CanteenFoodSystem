691d2692232309aa521af06f2595ec4f2a187370
Samiksha Garg <gargsamiksha0306@gmail.com>
2022-09-17 13:20:11 +0530

    initial item screen

---
+++ lib/screens/itemScreen/item_screen.dart
@@ -0,0 +1,64 @@
+import 'package:canteen_system/components/custom_app_bar.dart';
+import 'package:canteen_system/components/custom_navigation_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+import '../../helper/size_config.dart';
+
+class ItemScreen extends StatelessWidget {
+  const ItemScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+    return Scaffold(
+      appBar: customAppBar("Item"),
+      bottomNavigationBar: CustomBottomNavBar(),
+      body: Container(
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "MODAL BOTTOM SHEET EXAMPLE",
+              style: TextStyle(
+                  fontStyle: FontStyle.italic,
+                  letterSpacing: 0.4,
+                  fontWeight: FontWeight.w600),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            RaisedButton(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10.0))),
+              onPressed: () {
+                showModalBottomSheet(
+                    context: context,
+                    builder: (context) {
+                      return Container(
+                        height: 500,
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(50)),
+                      );
+                    });
+              },
+              padding:
+                  EdgeInsets.only(left: 30, right: 30, top: 15, bottom: 15),
+              color: Colors.pink,
+              child: Text(
+                'Click Me',
+                style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600,
+                    letterSpacing: 0.6),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
